2a716edc6162594222cfee82bc382216e4b442c1
Giuseppe <77988125+Rick-243@users.noreply.github.com>
2021-04-21 10:52:02 -0400

    Added js file updated link in root html file

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function listSelector(){
+    var ol = document.getElementsById("list_links");
+    const list = [
+        {
+            label: "Week01 notes",
+            url: "week01/index.html"
+        }
+    ]
+}
