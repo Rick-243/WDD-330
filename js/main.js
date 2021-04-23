4e07065cd3faaa97966ff11d1d52b67d3efee2e8
Giuseppe <77988125+Rick-243@users.noreply.github.com>
2021-04-23 12:20:02 -0400

    Changed js file

--- js/main.js
+++ js/main.js
@@ -1,12 +1,5 @@
 function listSelector(){
-    var a = document.createElement("a");
     var ol = document.getElementsById("list_links");
-    var li = document.createElement("li");
-    li.appendChild(document.createTextNode("Week02"));
-    a.textContent = "week02";
-    a.setAttribute('href', 'week02/index.html');
-    li.appendChild(a);
-    ol.appendChild(li);
     const list = [
         {
             label: "Week01",
