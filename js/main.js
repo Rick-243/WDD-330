f0a28af2ead8fae3202810e2095f97a4eb7757a3
Giuseppe <77988125+Rick-243@users.noreply.github.com>
2021-04-22 22:37:06 -0400

    Updated label

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ function listSelector(){
     var ol = document.getElementsById("list_links");
     const list = [
         {
-            label: "Week01 notes",
+            label: "Week01",
             url: "week01/index.html"
         }
     ]
