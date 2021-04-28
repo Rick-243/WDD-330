055588d7843f5d4c97fa763a8ee95ffa7eafa80b
Giuseppe <77988125+Rick-243@users.noreply.github.com>
2021-04-28 15:53:58 -0400

    Refactored code and added comments

--- week02/quiz.js
+++ week02/quiz.js
@@ -1,19 +1,40 @@
-function superQuiz(){
-    const quiz = [
-        ["What is Superman's real name?","Clark Kent"],
-        ["What is Wonder Woman's real name?","Diana Prince"],
-        ["What is Batman's real name?","Bruce Wayne"]
-    ];
-    let score = 0; 
-    for(const[question, answer] of quiz){
-        const response = prompt(question);
+const quiz = [
+    ["What is Superman's real name?","Clark Kent"],
+    ["What is Wonder Woman's real name?","Diana Prince"],
+    ["What is Batman's real name?","Bruce Wayne"]
+];
+function start(quiz){
+    let score = 0;
+    // main game loop
+    for(const [question,answer] of quiz){
+        const response = ask(question);
+        check(response,answer);
+    }
+    // end of main game loop
+    gameOver();
+    /*
+    The ask() , check() and gameOver() functions are 
+    defined at the end of the body of the start() function. 
+    They need to be placed inside the start() function as 
+    nested functions, as this gives them access to any 
+    variables defined inside the start() function's scope.
+    Because they are defined using function declarations, 
+    they are hoisted, so they can be defined after they are invoked.
+    */
+    // function declarations
+    function ask(question){
+        return prompt(question);
+    }
+    function check(response,answer){
         if(response === answer){
-            alert(`Correct!`);
-            score++;
-        }else{
-            alert(`Wrong! The correct answer was ${answer}.`);
+        alert('Correct!');
+        score++;
+        } else {
+        alert(`Wrong! The correct answer was ${answer}`);
         }
     }
-    // At the end of the game, report the player's score
-    alert(`Game Over, you scored ${score} point${score !== 1 ? 's' : ''}`);
+    function gameOver(){
+        alert(`Game Over, you scored ${score} point${score !== 1 ? 's' : ''}`);
+    }
 }
+start(quiz);
