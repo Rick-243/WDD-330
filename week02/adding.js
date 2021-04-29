6c9381f6efc61552a9271be88f111a01d6498279
Giuseppe <77988125+Rick-243@users.noreply.github.com>
2021-04-29 10:43:04 -0400

    Added stretch code

--- week02/adding.js
+++ week02/adding.js
@@ -19,7 +19,6 @@ function sum(number){
     }
     return total;
 }
-
 function final() {
     let input1 = parseFloat(document.getElementById("input").value);
     let input2 = parseFloat(document.getElementById("input2").value);
@@ -27,4 +26,36 @@ function final() {
     let  sum = input1 + input2 + input3;
     document.getElementById("output3").innerHTML = `the sum of the numbers you entered is: ` + sum;
 
+}
+// stretch solution
+function getNum(numId){
+    const number = document.getElementById(numId).value;
+    const numberFloat = parseFloat(number);
+    if (numberFloat !== NaN){
+      return numberFloat;
+    } else return 0;
+}
+function updateTotal(value){
+    const outputElement = document.getElementById('stretchOutput');
+    outputElement.innerHTML = 'Total: ' + value;
+}
+// function declaration
+function add2(num1, num2){
+    return num1 + num2;
+}
+// function expression
+const sub2 = function(num1, num2){
+    return num1 - num2;
+};
+// arrow function
+const mult2 = (num1, num2) => num1 * num2;
+
+// example of using a callback to pull it all together
+function performOperation(operation){
+    // the argument 'operation' is a function...so when we are ready we can call that function in our code below
+    const total = operation(
+        getNum('stretchNumber1'),
+        getNum('stretchNumber2')
+);
+updateTotal(total);
 }
