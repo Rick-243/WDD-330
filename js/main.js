584d191eafbe450bfc845e2404c0574093acb434
Giuseppe <77988125+Rick-243@users.noreply.github.com>
2021-04-29 22:32:54 -0400

    Changed code to auto populate the links

--- js/main.js
+++ js/main.js
@@ -1,12 +1,25 @@
 function listSelector(){
-    var ol = document.getElementsById("list_links");
+    var ol = document.getElementById("list_links");
     const list = [
         {
             label: "Week01",
-            url: "week01/index.html",
-
+            url: "week01/index.html"
+        },
+        {
             label: "Week02",
             url: "week02/index.html"
+        }, 
+        {
+            label: "Week03",
+            url: "week03/index.html"
         }       
     ]
+    for(let i = 0; i < list.length; i++){
+        let li = document.createElement("li");
+        let url = document.createElement("a");
+        url.setAttribute("href", list[i].url);
+        url.textContent = list[i].label;
+        li.append(url);
+        ol.append(li); 
+    }
 }
