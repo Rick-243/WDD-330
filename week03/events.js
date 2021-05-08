ab8c922c35e7bb1d24e12c25fbca99d5e4dbd080
Giuseppe <77988125+Rick-243@users.noreply.github.com>
2021-05-07 21:34:34 -0400

    added event handlers

--- week03/events.js
+++ week03/events.js
@@ -32,3 +32,59 @@ brokenLink.addEventListener('click',(event) => {
     event.preventDefault();
     console.log('Broken Link!');
 });
+
+// this code is for bubbling 
+ulElement = document.getElementById('list');
+liElement = document.querySelector('#list li');
+
+ulElement.addEventListener('click', (event) => 
+console.log('Clicked on ul') );
+
+liElement.addEventListener('click', (event) =>
+console.log('Clicked on li') );
+
+ulElement.addEventListener('click', highlight);
+
+/*
+// for capturing you must set the value of the 3rd parameter to true as shown below
+
+ulElement.addEventListener('click', (event) =>
+console.log('Clicked on ul'),true);
+
+liElement.addEventListener('click', (event) =>
+console.log('Clicked on li'),true);
+*/
+
+/*
+// for both use this code
+
+// capturing
+ulElement.addEventListener('click', (event) =>
+console.log('Clicked on ul'),true);
+
+liElement.addEventListener('click', (event) =>
+console.log('Clicked on li'),true);
+
+// bubbling
+ulElement.addEventListener('click', (event) =>
+console.log('Clicked on ul'),false );
+
+liElement.addEventListener('click', (event) =>
+console.log('Clicked on li'),false );
+*/
+
+/*
+The bubble phase can be stopped from occurring by adding the event.stopPropagation() method 
+into the callback function. In the following example, the event will fail to propagate as the 
+third argument is false , which stops capturing, and the event.stopPropagation() method is called, 
+which stops bubbling:
+
+liElement.addEventListener('click', (event) => {
+console.log('clicked on li');
+event.stopPropagation(); }, false);
+
+Be very wary of using the stopPropagation() method to stop the bubble phase occurring. 
+There may be other event listeners attached to elements further up the chain that won’t fire 
+as a result.
+
+*/
