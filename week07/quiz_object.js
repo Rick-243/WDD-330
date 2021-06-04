8e26d2cd09b15a2c414d591200e4f1373be69996
Giuseppe <77988125+Rick-243@users.noreply.github.com>
2021-06-04 18:13:10 -0400

    added json file

--- week07/quiz_object.js
+++
@@ -1,8 +0,0 @@
-const quiz = [
-    { name: "Superman",realName: "Clark Kent" },
-    { name: "Wonder Woman",realName: "Diana Prince" },
-    { name: "Batman",realName: "Bruce Wayne" },
-    { name: "The Hulk",realName: "Bruce Banner" },
-    { name: "Spider-man",realName: "Peter Parker" },
-    { name: "Cyclops",realName: "Scott Summers" }
-];
