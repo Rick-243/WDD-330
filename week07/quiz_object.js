eccf49561bd5de5270f2340f3e57c60dca4adec0
Giuseppe <77988125+Rick-243@users.noreply.github.com>
2021-06-04 17:51:33 -0400

    Week07 notes and code

---
+++ week07/quiz_object.js
@@ -0,0 +1,8 @@
+const quiz = [
+    { name: "Superman",realName: "Clark Kent" },
+    { name: "Wonder Woman",realName: "Diana Prince" },
+    { name: "Batman",realName: "Bruce Wayne" },
+    { name: "The Hulk",realName: "Bruce Banner" },
+    { name: "Spider-man",realName: "Peter Parker" },
+    { name: "Cyclops",realName: "Scott Summers" }
+];
